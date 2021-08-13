6060c5979f213c8755edb02971cfaa7573a064d7
yassine aourhebal <aourhebal.yassine@gmail.com>
2021-08-13 14:11:44 +0100

    add middlewares

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+app.get('/', (req, res) => res.send('freelance test app'));
 
 const PORT = process.env.PORT || 5000;
 
