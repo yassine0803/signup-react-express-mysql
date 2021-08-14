f321e3a33eecb5e1915c08b074d2390163e4cdcb
yassine aourhebal <aourhebal.yassine@gmail.com>
2021-08-14 16:25:02 +0100

    adding Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import Signup from './components/signup/Signup';
+import Profile from './components/profile/Profile';
+import {useState} from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 function App() {
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
   return (
     <div className="App">
-      <Signup />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Signup} />
+          <Route path="/profile" exact component={() => user ? <Profile /> :<Redirect to="/" />} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
