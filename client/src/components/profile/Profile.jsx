ff1e8338f5f6920762a46abcaf457836660d2fbc
yassine aourhebal <aourhebal.yassine@gmail.com>
2021-08-17 17:30:13 +0100

    update Profile function to class component

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -1,127 +1,133 @@
-import { useState, useEffect } from 'react';
-import { useParams } from "react-router-dom";
+import React,{Component} from 'react';
 import camera from '../../images/camera_white.png';
 import draw from '../../images/draw.png';
 import check from '../../images/check.png';
 import { getData, uploadImage, postData, updateData, url } from '../../api';
 import styles from './Profile.module.css';
+import { withRouter } from "react-router";
 
-const Profile = () => {
-    const { id } = useParams();
-    const [edit, setEdit] = useState(false);
-    const [user, setUser] = useState({});
-    const [loading, setLoading] = useState(true);
-    const [name, setName] = useState(false);
-    const [username, setUsername] = useState(false);
-    const [newUser, setNewUser] = useState(true);
-    const [profileGallery, setProfileGallery] = useState([]);
+class Profile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: props.match.params.id,
+            edit: false,
+            user: {},
+            loading: false,
+            name: false,
+            username: false,
+            newUser: true,
+            profileGallery: []
+        }
+    }
 
-    const checkUsername = async () => {
+    checkUsername = async () => {
         console.log('Checking');
-        const result = await postData('/users/check-user', { username: user.username });
+        const result = await postData('/users/check-user', { username: this.state.user.username });
         if (result.status !== 201)
-            setNewUser(false)
+            this.setState({...this.state, newUser: false});
         else
-            setNewUser(true)
+        this.setState({...this.state, newUser: true});
     }
 
-    const fetchUser = async () => {
-        const { data } = await getData('/users/' + id);
-        setUser(data);
-        setLoading(false);
+    fetchUser = async () => {
+        const { data } = await getData('/users/' + this.state.id);
+        this.setState({...this.state, loading: false, user: data});
     }
 
-    const fetUsergallery = async () => {
-        const { data } = await getData('/images/gallery/user/' + id);
-        setProfileGallery(data);
+    fetUsergallery = async () => {
+        const { data } = await getData('/images/gallery/user/' + this.state.id);
+        this.setState({...this.state, profileGallery: data});
     }
 
-    const handleChangeInput = (e) => {
+    handleChangeInput = (e) => {
         if (e.target.value.length)
-            setUser(values => ({ ...values, [e.target.name]: e.target.value }));
+            this.setState({...this.state, user: { ...this.state.user, [e.target.name]: e.target.value }});
     }
-    const hundleUploadImage = async (e, currentImage) => {
+    hundleUploadImage = async (e, currentImage) => {
         const { data } = await uploadImage('/images/edit-profile-image/' + currentImage, e);
-        setUser(oldvalues => ({ ...oldvalues, profileImg: data.filename }))
+        this.setState({...this.state, user: { ...this.state.user, profileImg: data.filename}});
 
     }
-    const hundleUploadGallery = async (e, currentImage) => {
+    hundleUploadGallery = async (e, currentImage) => {
         const { data } = await uploadImage('/images/edit-gallery-image/' + currentImage, e);
-        let objIndex = profileGallery.findIndex((obj => obj.image === currentImage));
-        profileGallery[objIndex] = { image: data.filename };
-        setProfileGallery([]);
-        setProfileGallery(profileGallery);
+        let objIndex = this.state.profileGallery.findIndex((obj => obj.image === currentImage));
+        this.state.profileGallery[objIndex] = { image: data.filename };
+        this.setState({...this.state, profileGallery: this.state.profileGallery});
     }
 
-    const hundleSubmit = async () => {
-        if (newUser) {
-            const { status } = await updateData('/users/' + user.id, user);
+    hundleSubmit = async () => {
+        if (this.state.newUser) {
+            const { status } = await updateData('/users/' + this.state.user.id, this.state.user);
             if (status === 201) {
-                fetchUser();
-                setEdit(false);
+                this.fetchUser();
+                this.setState({...this.state, edit: false});
             }
             else {
                 console.log('error');
             }
         }
     }
-    useEffect(() => {
-        fetchUser();
-        fetUsergallery();
-    }, [id]);
-    return (
-        <div className={styles.root}>
-            <h1 className={styles.profile__title}>
-                Profile
-            </h1>
-            {user.name && <div className={styles.profile}>
-                <div className={styles.profile__avatar}>
-                    {edit && <div className={styles.avatar_edit} >
-                        <input type='file' className={styles.edit_input} id="profileImg" accept=".png, .jpg, .jpeg" onChange={(e) => hundleUploadImage(e, user.profileImg)} />
-                        <label className={styles.edit_label} htmlFor="profileImg">
-                            <img className={styles.avatar_upload} src={camera} alt="" />
-                        </label>
-                    </div>}
-                    <div className={edit ? styles.avatar_preview_new : styles.avatar_preview}>
-                        {
-                            user?.profileImg
-                            &&
-                            <img className={edit ? styles.avatar_iamge_edit : styles.avatar_iamge} src={`${url}/uploads/${user.profileImg}`} alt="" />
-                        }
-                    </div>
-                </div>
-                <div className={styles.profile_name}>
-                    {!name ? <span className={styles.input_preview}>{user?.name}</span> : <input name="name" placeholder={user.name} className={styles.input_edit} onBlur={() => setName(false)} onChange={handleChangeInput} />}
-                    {edit && <img className={styles.image_upload} src={name ? check : draw} alt="" onClick={() => { setName(!name); setUsername(false) }} />}
-                </div>
-                <div className={styles.profile_username}>
-                    {!username ? <span className={styles.input_preview}>@{user?.username}</span> : <input name="username" placeholder={user.username} className={newUser ? styles.input_edit : styles.input_edit_error} onChange={handleChangeInput} onBlur={() => { checkUsername(); setUsername(false) }} />}
-                    {edit && <img className={styles.image_upload} src={username ? check : draw} alt="" onClick={() => { setUsername(!username); setName(false) }} />}
-                    {!newUser && <div className={styles.username_error}>Username already taken</div>}
-                </div>
-                <button className={styles.edit_button} onClick={() => edit ? hundleSubmit() : setEdit(true)}>Edit Profile</button>
-            </div>}
-            {profileGallery && profileGallery.length > 0
-                &&
-                <div className={styles.profile__gallery}>
-
-                    {profileGallery.map((img) => (
-                        <div key={img.image} className={styles.image_container}>
-                            {edit && <div className={styles.gallery_edit}>
-                                <input type='file' className={styles.gallery_input} id={img.image} accept=".png, .jpg, .jpeg" onChange={(e) => hundleUploadGallery(e, img.image)} />
-                                <label className={styles.gallery_label} htmlFor={img.image}>
-                                    <img className={styles.image_upload} src={draw} alt="" />
-                                </label>
-                            </div>}
-                            <img src={`${url}/uploads/${img.image}`} alt="" className={styles.gallery_image} />
+    componentDidMount(){
+        this.fetchUser();
+        this.fetUsergallery();
+    }
+    render() {
+        
+        return (
+            <div className={styles.root}>
+                <h1 className={styles.profile__title}>
+                    Profile
+                </h1>
+                {this.state.user.name && <div className={styles.profile}>
+                    <div className={styles.profile__avatar}>
+                        {this.state.edit && <div className={styles.avatar_edit} >
+                            <input type='file' className={styles.edit_input} id="profileImg" accept=".png, .jpg, .jpeg" onChange={(e) => this.hundleUploadImage(e, this.state.user.profileImg)} />
+                            <label className={styles.edit_label} htmlFor="profileImg">
+                                <img className={styles.avatar_upload} src={camera} alt="" />
+                            </label>
+                        </div>}
+                        <div className={this.state.edit ? styles.avatar_preview_new : styles.avatar_preview}>
+                            {
+                                this.state.user?.profileImg
+                                &&
+                                <img className={this.edit ? styles.avatar_iamge_edit : styles.avatar_iamge} src={`${url}/uploads/${this.state.user.profileImg}`} alt="" />
+                            }
                         </div>
-                    ))}
-
-                </div>
-            }
-            {loading && <div className={styles.profil__loader}></div>}
-        </div>
-    );
+                    </div>
+                    <div className={styles.profile_name}>
+                        {!this.state.name ? <span className={styles.input_preview}>{this.state.user?.name}</span> : <input name="name" placeholder={this.state.user.name} className={styles.input_edit} onBlur={() => this.setState({...this.state, name: false})} onChange={this.handleChangeInput} />}
+                        {this.state.edit && <img className={styles.image_upload} src={this.state.name ? check : draw} alt="" onClick={() => {this.setState({...this.state, name: !this.state.name, username: false})}} />}
+                    </div>
+                    <div className={styles.profile_username}>
+                        {!this.state.username ? <span className={styles.input_preview}>@{this.state.user?.username}</span> : <input name="username" placeholder={this.state.user.username} className={this.state.newUser ? styles.input_edit : styles.input_edit_error} onChange={this.handleChangeInput} onBlur={() => { this.checkUsername(); this.setState({...this.state, username: false}) }} />}
+                        {this.state.edit && <img className={styles.image_upload} src={this.state.username ? check : draw} alt="" onClick={() => { this.setState({...this.state, username: !this.state.username, name: false}) }} />}
+                        {!this.state.newUser && <div className={styles.username_error}>Username already taken</div>}
+                    </div>
+                    <button className={styles.edit_button} onClick={() => this.state.edit ? this.hundleSubmit() : this.setState({...this.state, edit: true})}>Edit Profile</button>
+                </div>}
+                {this.state.profileGallery && this.state.profileGallery.length > 0
+                    &&
+                    <div className={styles.profile__gallery}>
+    
+                        {this.state.profileGallery.map((img) => (
+                            <div key={img.image} className={styles.image_container}>
+                                {this.state.edit && <div className={styles.gallery_edit}>
+                                    <input type='file' className={styles.gallery_input} id={img.image} accept=".png, .jpg, .jpeg" onChange={(e) => this.hundleUploadGallery(e, img.image)} />
+                                    <label className={styles.gallery_label} htmlFor={img.image}>
+                                        <img className={styles.image_upload} src={draw} alt="" />
+                                    </label>
+                                </div>}
+                                <img src={`${url}/uploads/${img.image}`} alt="" className={styles.gallery_image} />
+                            </div>
+                        ))}
+    
+                    </div>
+                }
+                {this.state.loading && <div className={styles.profil__loader}></div>}
+            </div>
+        );
+    }
 }
 
-export default Profile;
+export default withRouter(Profile);
